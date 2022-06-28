1ca920953ced9b007cf3752151bd4a11cb9aa00c
tmenture <thomasoxemail@gmail.com>
2022-06-28 11:36:53 -0400

    added reply capabilities, routes, and connected to the front end

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const {
     addComment,
-    removeComment
+    removeComment,
+    addReply,
+    removeReply
 } = require('../../controllers/comment-controller');
 
 // posts comment
 router.route('/:pizzaId').post(addComment);
 // deletes comment
 router.route('/:pizzaId/:commentId').delete(removeComment);
+// adds reply
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
+// remove reply
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
